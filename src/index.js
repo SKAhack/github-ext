4228d162e60c8c79a951a350ce54dcd837b882a1
Hiroki Sato <m@skahack.com>
2015-12-20 11:48:16 +0900

    v0.1.0

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import injection from 'github-injection';
-import State from './state';
-import Plugin from './plugin';
+import init from 'github-ext-core';
+import DontDeleteReleaseBranch from './plugins/dont-delete-release-branch';
+import DontMerge from './plugins/dont-merge';
+import Trello from './plugins/trello';
 
-const state = new State();
-const url = 'https://gist.githubusercontent.com/SKAhack/bc9b3d25a3618d09132a/raw/07cf45b55b453469c46c8780c0f174261b4af42c/setting.json';
-
-let jsonCache;
-
-const fetchSetting = (url, callback) => {
-  if (jsonCache) {
-    callback(jsonCache);
-    return;
-  }
-
-  fetch(url).then(res => {
-    res.json().then(json => {
-      jsonCache = json;
-      callback(jsonCache);
-    });
-  });
+const url = 'https://gist.githubusercontent.com/SKAhack/bc9b3d25a3618d09132a/raw/setting.json';
+const plugins = {
+  DontDeleteReleaseBranch,
+  DontMerge,
+  Trello
 };
 
-injection(window, () => {
-  state.init();
-
-  fetchSetting(url, (setting) => {
-    if (state.page) {
-      new Plugin(state.page, setting);
-    }
-  });
-});
+init(window, url, plugins);
